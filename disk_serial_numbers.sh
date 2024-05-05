f62aa92d842246fe323fc1addd4366a0a9f86768
Marius Manea <maneamarius@gmail.com>
2024-05-05 17:06:56 +0300

    improve disk_serial_numbers.sh

--- disk_serial_numbers.sh
+++ disk_serial_numbers.sh
@@ -1,5 +1,7 @@
 #!/bin/env bash
 
+set -e
+
 sudo apt install smartmontools btrfs-progs
 
 file_name=disk_serial_numbers.txt
@@ -8,8 +10,7 @@ file_name=disk_serial_numbers.txt
 disks=$( sudo lshw -class disk | grep 'logical name' | grep '/dev' | grep -v nvme | grep -v ng | cut -d':' -f2 | cut -d ' ' -f2 )
 
 for disk in ${disks[@]}; do
-    partitions=$(ls -la ${disk}* | wc -l)
-    if [[ "$partitions" != "1" ]]; then
+    if [[ -n "$(lsblk -nd -o PTTYPE "$disk" 2>/dev/null)" ]]; then
         echo "WARN: Skipping disk '$disk' because it has partitions on it .."
     else
         #    serial_number=$(udevadm info --query=all --name=$disk | grep ID_SERIAL_SHORT | cut -d'=' -f2)
@@ -26,22 +27,22 @@ for disk in ${disks[@]}; do
                 fs_type=$(echo $type | tr -d '"')
                 echo "fs_type: $fs_type"
                 if [[ "$fs_type" != "btrfs" ]]; then
-                    echo "WARN: disk '$disk' with serial number '$serial_number' has FS type '$fs_type'!"
-                    sudo mkfs.btrfs -f $disk
+                    echo "ERR: disk '$disk' with serial number '$serial_number' has FS type '$fs_type'!"
+                    exit 1
                 fi
             fi
         done
-        sudo btrfs filesystem label $disk $serial_number
+        sudo btrfs filesystem label $disk $serial_number || true
     fi
 done
 
 for disk in ${disks[@]}; do\
     partitions=$(ls -la ${disk}* | wc -l)
-    if [[ "$partitions" == "1" ]]; then
+    if [[ -z "$(lsblk -nd -o PTTYPE "$disk" 2>/dev/null)" ]]; then
         serial_number=$(sudo smartctl -i $disk | grep -i 'Serial number' | cut -d':' -f 2 | tr -d ' ')
         echo -e "LABEL=$serial_number\t/mnt/chia/$serial_number\t\tbtrfs\tdefaults,compress=none,nofail\t0\t0" >> /etc/fstab
-        mount /mnt/chia/$serial_number
-        mkdir -p /mnt/chia/$serial_number
+        mount /mnt/chia/$serial_number || true
         chown ubuntu:ubuntu -R /mnt/chia/$serial_number
+        chmod 777 -R /mnt/chia/$serial_number
     fi
 done
