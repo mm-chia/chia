c03c178eabb66017b7a4876de9e0d85bd2247796
Marius Manea <marius@manea.org>
2024-06-24 12:50:09 +0300

    update setup-tools.sh

--- setup-tools.sh
+++ setup-tools.sh
@@ -1,1 +1,1 @@
-sudo apt install git mc terminator ncdu vim gparted nvtop htop ifenslave net-tools smartmontools btrfs-progs xfsprogs python3-pip gnome-disk-utility -y
+sudo apt install git mc terminator ncdu vim gparted nvtop htop ifenslave net-tools smartmontools btrfs-progs xfsprogs python3-pip jq -y
