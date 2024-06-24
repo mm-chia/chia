183fc77656c1f59d996d5110271d35abff91b78a
Marius Manea <marius@manea.org>
2024-06-24 12:44:54 +0300

    update gigahorse_pos.sh

--- gigahorse_pos.sh
+++ gigahorse_pos.sh
@@ -1,3 +1,103 @@
-./ProofOfSpace farm -r 8 --diff 10000 -f /mnt/ramdisk0/plot-k32-c29-2024-04-14-09-57-56ca3d9bfb59499b9cfa7e1cf3a1a1beba1313639bb2d4ddfd3b2663c24fffce.plot 100
+./ProofOfSpace farm -r 16 --diff 10000 -f /mnt/chia/plot-k32-c29-2024-04-14-16-26-ec740750cea01c427321106487e446ad62c38ac3aa88692ee5ff279d24a68bf1.plot 1000
 
-./ProofOfSpace farm -r 8 --diff 20000 -f /mnt/ramdisk0/plot-k32-c30-2024-04-13-20-42-c0d49926c1b146487cd2f5e0b2fafa45e047b87435b928faa2696fafa2a1c8c9.plot 100
+./ProofOfSpace farm -r 16 --diff 20000 -f /mnt/chia/plot-k32-c30-2024-04-14-16-37-54b943750ae67d1698df17ecf57bb65496fbba892ac20bd48b7af53df3f49f23.plot 1000
+
+exit 0
+
+# RESULTS #
+
+# RTX 4060 C29
+operation: farm
+threads: 16
+iterations: 1000
+[chiapos] Using 16 / 32 CPU threads
+[chiapos] Using 1 / 1 CUDA devices
+Total success:  1031 / 1000, 103.1 %
+Total failures: 0 / 1000, 0 %
+Total filtered: 1031 / 1031, 100 %
+Partial Difficulty: 10000 (0.00555701 % chance)
+Max Farm Size @512: 1.19638 PiB (physical)
+Max Farm Size @256: 0.598192 PiB (physical)
+Max Farm Size @128: 0.299096 PiB (physical)
+Average time to compute quality: 0.166545 sec
+Maximum time to compute full proof: 0 sec
+
+# RTX 4060 C30
+operation: farm
+threads: 16
+iterations: 1000
+[chiapos] Using 16 / 32 CPU threads
+[chiapos] Using 1 / 1 CUDA devices
+Total success:  993 / 1000, 99.3 %
+Total failures: 0 / 1000, 0 %
+Total filtered: 993 / 993, 100 %
+Partial Difficulty: 20000 (0.00277851 % chance)
+Max Farm Size @512: 0.747955 PiB (physical)
+Max Farm Size @256: 0.373977 PiB (physical)
+Max Farm Size @128: 0.186989 PiB (physical)
+Average time to compute quality: 0.240308 sec
+Maximum time to compute full proof: 0 sec
+
+# RTX A2000 C29
+operation: farm
+threads: 16
+iterations: 1000
+[chiapos] Using 16 / 32 CPU threads
+[chiapos] Using 1 / 1 CUDA devices
+Total success:  1031 / 1000, 103.1 %
+Total failures: 0 / 1000, 0 %
+Total filtered: 1031 / 1031, 100 %
+Partial Difficulty: 10000 (0.00555701 % chance)
+Max Farm Size @512: 0.870848 PiB (physical)
+Max Farm Size @256: 0.435424 PiB (physical)
+Max Farm Size @128: 0.217712 PiB (physical)
+Average time to compute quality: 0.228802 sec
+Maximum time to compute full proof: 0 sec
+
+# RTX A2000 C30
+operation: farm
+threads: 16
+iterations: 1000
+[chiapos] Using 16 / 32 CPU threads
+[chiapos] Using 1 / 1 CUDA devices
+Total success:  993 / 1000, 99.3 %
+Total failures: 0 / 1000, 0 %
+Total filtered: 993 / 993, 100 %
+Partial Difficulty: 20000 (0.00277851 % chance)
+Max Farm Size @512: 0.54003 PiB (physical)
+Max Farm Size @256: 0.270015 PiB (physical)
+Max Farm Size @128: 0.135007 PiB (physical)
+Average time to compute quality: 0.332833 sec
+Maximum time to compute full proof: 0 sec
+
+# RTX A4000 C29
+operation: farm
+threads: 16
+iterations: 1000
+[chiapos] Using 16 / 32 CPU threads
+[chiapos] Using 1 / 1 CUDA devices
+Total success:  1031 / 1000, 103.1 %
+Total failures: 0 / 1000, 0 %
+Total filtered: 1031 / 1031, 100 %
+Partial Difficulty: 10000 (0.00555701 % chance)
+Max Farm Size @512: 1.6715 PiB (physical)
+Max Farm Size @256: 0.835749 PiB (physical)
+Max Farm Size @128: 0.417874 PiB (physical)
+Average time to compute quality: 0.119206 sec
+Maximum time to compute full proof: 0 sec
+
+# RTX A4000 C30
+operation: farm
+threads: 16
+iterations: 1000
+[chiapos] Using 16 / 32 CPU threads
+[chiapos] Using 1 / 1 CUDA devices
+Total success:  993 / 1000, 99.3 %
+Total failures: 0 / 1000, 0 %
+Total filtered: 993 / 993, 100 %
+Partial Difficulty: 20000 (0.00277851 % chance)
+Max Farm Size @512: 1.02616 PiB (physical)
+Max Farm Size @256: 0.513081 PiB (physical)
+Max Farm Size @128: 0.256541 PiB (physical)
+Average time to compute quality: 0.175157 sec
+Maximum time to compute full proof: 0 sec
