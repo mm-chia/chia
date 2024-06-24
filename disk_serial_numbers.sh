834c35ca55f977406ece2afbe4ae24e22756ba2d
Marius Manea <marius@manea.org>
2024-06-24 14:03:14 +0300

    update scripts

--- disk_serial_numbers.sh
+++ disk_serial_numbers.sh
@@ -2,23 +2,24 @@
 
 set -e
 
-sudo apt install smartmontools btrfs-progs
+sudo apt install smartmontools btrfs-progs jq
 
-file_name=disk_serial_numbers.txt
-
-
-disks=$( sudo lshw -class disk | grep 'logical name' | grep '/dev' | grep -v nvme | grep -v ng | cut -d':' -f2 | cut -d ' ' -f2 )
-
-for disk in ${disks[@]}; do
-    if [[ -n "$(lsblk -nd -o PTTYPE "$disk" 2>/dev/null)" ]]; then
-        echo "WARN: Skipping disk '$disk' because it has partitions on it .."
+user=$USER
+for row in $(sudo lshw -class disk -json | jq -r '.[] | @base64'); do
+    _jq() {
+        echo ${row} | base64 --decode | jq -r ${1}
+    }
+    disk=$(_jq '.logicalname')
+    serial=$(_jq '.serial')
+    mounted=$(_jq '.configuration.state')
+    if [[ "$mounted" == "mounted" ]]; then
+      echo "WARN: Skipping disk with serial number '$serial' because it is mounted .."
     else
-        #    serial_number=$(udevadm info --query=all --name=$disk | grep ID_SERIAL_SHORT | cut -d'=' -f2)
-        serial_number=$(sudo smartctl -i $disk | grep -i 'Serial number' | cut -d':' -f 2 | tr -d ' ')
-        echo "  disk: $disk | serial_number: $serial_number"
-        mkdir -p /mnt/chia/$serial_number
-        chmod 777 -R /mnt/chia/$serial_number
-        chown ubuntu:ubuntu -R /mnt/chia/$serial_number
+      if sudo sfdisk -d $disk 2>&1 | grep "does not contain a recognized partition table" &>/dev/null; then
+        echo "  disk: $disk | serial: $serial"
+        mkdir -p /mnt/chia/$serial
+        chmod 777 -R /mnt/chia/$serial
+        chown ubuntu:ubuntu -R /mnt/chia/$serial
         read -ra values <<< "$(sudo blkid -p $disk)"
         for value in "${values[@]}"; do
             echo $value
@@ -27,22 +28,21 @@ for disk in ${disks[@]}; do
                 fs_type=$(echo $type | tr -d '"')
                 echo "fs_type: $fs_type"
                 if [[ "$fs_type" != "btrfs" ]]; then
-                    echo "ERR: disk '$disk' with serial number '$serial_number' has FS type '$fs_type'!"
+                    echo "ERR: disk '$disk' with serial number '$serial' has FS type '$fs_type'!"
                     exit 1
                 fi
             fi
         done
-        sudo btrfs filesystem label $disk $serial_number || true
+        sudo btrfs filesystem label $disk $serial
+        echo -e "LABEL=$serial\t/mnt/chia/$serial\t\tbtrfs\tdefaults,compress=none,nofail\t0\t0" | sudo tee -a /etc/fstab
+        sudo mkdir -p /mnt/chia/$serial
+        sudo chown $user:$user -R /mnt/chia/$serial
+        sudo chmod 777 -R /mnt/chia/$serial
+        sudo mount /mnt/chia/$serial
+      else
+        echo "WARN: Skipping disk '$disk | $serial' because it has partitions on it .."
+      fi
     fi
 done
-
-for disk in ${disks[@]}; do\
-    partitions=$(ls -la ${disk}* | wc -l)
-    if [[ -z "$(lsblk -nd -o PTTYPE "$disk" 2>/dev/null)" ]]; then
-        serial_number=$(sudo smartctl -i $disk | grep -i 'Serial number' | cut -d':' -f 2 | tr -d ' ')
-        echo -e "LABEL=$serial_number\t/mnt/chia/$serial_number\t\tbtrfs\tdefaults,compress=none,nofail\t0\t0" >> /etc/fstab
-        mount /mnt/chia/$serial_number || true
-        chown ubuntu:ubuntu -R /mnt/chia/$serial_number
-        chmod 777 -R /mnt/chia/$serial_number
-    fi
-done
+sudo chown $user:$user -R /mnt/chia/
+sudo chmod 777 -R /mnt/chia/
--- setup-tools.sh
+++ setup-tools.sh
@@ -1,1 +1,1 @@
-sudo apt install git mc terminator ncdu vim gparted nvtop htop ifenslave net-tools smartmontools btrfs-progs xfsprogs python3-pip jq -y
+sudo apt install git mc terminator ncdu vim gparted nvtop htop ifenslave net-tools smartmontools btrfs-progs xfsprogs python3-pip jq gnome-disk-utility -y
